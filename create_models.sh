4ee3ff0b05e9e5558d24fe978ecb6622f645b64a
zonkiie <33644484+zonkiie@users.noreply.github.com>
2018-04-24 03:35:34 +0200

    Update create_models.sh

--- create_models.sh
+++ create_models.sh
@@ -6,5 +6,5 @@ buffalo db generate model User Username:text Email:text Passwordhash:text Book:[
 #soda create -d database.yml
 #buffalo db create -a
 
-sqlite3 db.sqlite "create table book(id text primary key, created_at datetime, updated_at datetime, name text, user_id text);"
 sqlite3 db.sqlite "create table user(id text primary key, created_at datetime, updated_at datetime, username text, email text, passwordhash text);"
+sqlite3 db.sqlite "create table book(id text primary key, created_at datetime, updated_at datetime, name text, user_id text, FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE RESTRICT ON UPDATE RESTRICT);"
